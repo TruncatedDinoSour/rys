da108ea4cdee6e926faad01efe621db29cef4b44
Ari Archer <truncateddinosour@gmail.com>
2022-02-19 07:22:42 +0200

    Added -quiet flag and added do_msg in side-effect having functions
    Signed-off-by: Ari Archer <truncateddinosour@gmail.com>

--- scripts/full_tests.sh
+++ scripts/full_tests.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
 main() {
-    ./scripts/tests.sh
+    ./scripts/tests.sh "$@"
 
     printf '****** Press enter to start failing tests testing ******'
     read -r
 
-    NOFAIL=1 TESTDIR=tests_fail ./scripts/tests.sh
+    NOFAIL=1 TESTDIR=tests_fail ./scripts/tests.sh "$@"
 
     printf '****** Press enter to start example testing ******'
     read -r
 
-    TESTDIR=examples ./scripts/tests.sh
+    TESTDIR=examples ./scripts/tests.sh "$@"
 }
 
 main "$@"
