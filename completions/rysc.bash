c7592e5e4e84fde1523bcfcf5cff76755d2b65bc
Ari Archer <ari.web.xyz@gmail.com>
2022-04-23 17:54:28 +0300

    Fix completion
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- completions/rysc.bash
+++ completions/rysc.bash
@@ -1,15 +1,15 @@
 # rysc(1) completion                                    -*- shell-script -*-
 
 _rysc() {
-    local words cword cmd
+    local words cword cmd cur
     _init_completion -s || return
 
     for ((cmd = 1; cmd <= cword; cmd++)); do
         if [[ ${words[cmd]} == -* ]]; then
-            COMPREPLY=(-help -debug -release
-                -linux-elf64-x86_64-nasm -run --
-                -o -strip -quiet -verbose
-                -typecheck -no-typecheck)
+            COMPREPLY=($(compgen -W "-help -debug -release\
+                -linux-elf64-x86_64-nasm -run --\
+                -o -strip -quiet -verbose\
+                -typecheck -no-typecheck" -- "$cur"))
         fi
     done
 } && complete -F _rysc -o bashdefault -o default rysc
